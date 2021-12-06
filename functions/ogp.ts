9dc4ce2b64fa2adb480a9a7faf0ed31abac56629
K <k.rev.urtica@gmail.com>
2021-12-06 10:24:09 +0900

    🚧 fix: ogp動作確認

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -7,46 +7,45 @@ import sharp from 'sharp';
 // eslint-disable-next-line require-await
 const ogpHandler: Handler = async (e) => {
   const param = e.rawQuery;
-  let image;
-  try {
-    const svg = fs.readFileSync(path.resolve(__dirname, '../assets/img/map-mobile.svg'));
 
-    const svgWk = svg.toString('utf-8');
+  const svg = fs.readFileSync(path.resolve(__dirname, '../assets/img/map-mobile.svg'));
 
-    let svgString = '';
-    for (let s of svgWk.split(/\r\n|\n/)) {
-      // if (s.includes('geolonia-svg-map')) {
-      //   s = s.replace(
-      //     '>',
-      //     ' style="background-image: linear-gradient(135deg, #97ABFF 10%, #123597 100%)">'
-      //   );
-      // }
+  const svgWk = svg.toString('utf-8');
 
-      if (s.includes('data-code')) {
-        s = s.replace(/fill="(.*?)"/, 'fill="#f8bbd0"');
-      }
-      svgString += s;
+  let svgString = '';
+  for (let s of svgWk.split(/\r\n|\n/)) {
+    // if (s.includes('geolonia-svg-map')) {
+    //   s = s.replace(
+    //     '>',
+    //     ' style="background-image: linear-gradient(135deg, #97ABFF 10%, #123597 100%)">'
+    //   );
+    // }
+
+    if (s.includes('data-code')) {
+      s = s.replace(/fill="(.*?)"/, 'fill="#f8bbd0"');
     }
-
-    image = await sharp(Buffer.from(svgString))
-      .flatten({ background: '#fefefe' })
-      .png()
-      .resize({
-        width: 1920,
-        height: 1005,
-        // fit: 'fill',
-        fit: 'contain',
-        background: '#fefefe',
-      })
-      .toBuffer();
-  } catch (err) {
-    console.log(err);
+    svgString += s;
   }
+
+  const image = await sharp(Buffer.from(svgString))
+    .flatten({ background: '#fefefe' })
+    .png()
+    .resize({
+      width: 1200,
+      height: 630,
+      // fit: 'fill',
+      fit: 'contain',
+      background: '#fefefe',
+    })
+    .toBuffer();
+
+  console.log('ret image');
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'image/png',
+      'Content-Length': image?.length,
     },
     body: image?.toString('base64'),
     isBase64Encoded: true,
