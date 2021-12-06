36cf9ef8d84f3e9931033cccd09f54abe5f8032f
K <k.rev.urtica@gmail.com>
2021-12-06 14:52:44 +0900

    🔧 fix: Add Access-Control-Allow-Origin

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -44,6 +44,7 @@ const ogpHandler: Handler = async (e) => {
   return {
     statusCode: 200,
     headers: {
+      'Access-Control-Allow-Origin': '*',
       'Accept-Ranges': 'bytes',
       'Content-Type': 'image/png',
       'Content-Length': image?.length,
