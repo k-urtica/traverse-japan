4810ac30c9c44bdbbaeb74038e05dca469a0b4b9
K <k.rev.urtica@gmail.com>
2021-12-09 19:27:14 +0900

    ✨ feat: URLに応じた動的OGP画像生成を追加

--- functions/ogp.ts
+++ functions/ogp.ts
@@ -3,43 +3,70 @@ import * as path from 'path';
 import { builder, Handler } from '@netlify/functions';
 
 import sharp from 'sharp';
+import TextToSVG from 'text-to-svg';
+
+const FONT = path.resolve(__dirname, '../assets/font/Mamelon-for-ogp.otf');
 
 // eslint-disable-next-line require-await
 const ogpHandler: Handler = async (e) => {
   const param = e.rawQuery;
+  const codes = param.replace('pref=', '').match(/.{2}/g);
 
   const svg = fs.readFileSync(path.resolve(__dirname, '../assets/img/map-mobile.svg'));
-
-  const svgWk = svg.toString('utf-8');
+  const svgWkString = svg.toString('utf-8');
 
   let svgString = '';
-  for (let s of svgWk.split(/\r\n|\n/)) {
-    // if (s.includes('geolonia-svg-map')) {
-    //   s = s.replace(
-    //     '>',
-    //     ' style="background-image: linear-gradient(135deg, #97ABFF 10%, #123597 100%)">'
-    //   );
-    // }
-
-    if (s.includes('data-code')) {
-      s = s.replace(/fill="(.*?)"/, 'fill="#f8bbd0"');
+  if (codes !== null) {
+    let dc;
+    for (let s of svgWkString.split(/\r\n|\n/)) {
+      dc = s.match(/data-code="[0-9]{2}/);
+      // urlクエリに存在するcodeの場合、該当のdata-code行のfill styleを変更する
+      if (dc !== null && codes.includes(dc[0].slice(-2))) {
+        s = s.replace(/fill="(.*?)"/, 'fill="#f8bbd0"');
+      }
+      svgString += s;
     }
-    svgString += s;
+  } else {
+    svgString = svgWkString;
   }
 
-  const image = await sharp(Buffer.from(svgString))
-    .flatten({ background: '#fefefe' })
-    .png()
+  const map = await sharp(Buffer.from(svgString))
     .resize({
-      width: 1200,
-      height: 630,
-      // fit: 'fill',
-      fit: 'contain',
-      background: '#fefefe',
+      height: 600,
+      fit: 'cover',
+    })
+    .extend({
+      right: 65,
+      background: { r: 0, g: 0, b: 0, alpha: 0 },
     })
     .toBuffer();
 
-  console.log('ret image');
+  const prefCnt = codes?.length || 0;
+  const text1 = textToSVG(`${prefCnt}都道府県を踏破！`, 52);
+  const text2 = textToSVG(`日本の${Math.floor((prefCnt / 47) * 100)}％`, 52);
+
+  const base = path.resolve(__dirname, '../assets/img/base.png');
+  const ogpImage = await sharp(base)
+    .composite([
+      {
+        input: map,
+        blend: 'over',
+        gravity: 'east',
+      },
+      {
+        input: Buffer.from(text1),
+        blend: 'over',
+        top: 200,
+        left: 65,
+      },
+      {
+        input: Buffer.from(text2),
+        blend: 'over',
+        top: 270,
+        left: 60,
+      },
+    ])
+    .toBuffer();
 
   return {
     statusCode: 200,
@@ -47,11 +74,22 @@ const ogpHandler: Handler = async (e) => {
       'Access-Control-Allow-Origin': '*',
       'Accept-Ranges': 'bytes',
       'Content-Type': 'image/png',
-      'Content-Length': image?.length,
+      'Content-Length': ogpImage?.length,
     },
-    body: image?.toString('base64'),
+    body: ogpImage?.toString('base64'),
     isBase64Encoded: true,
   };
+};
+
+const textToSVG = (text: string, fontSize: number) => {
+  const svgOptions: TextToSVG.GenerationOptions = {
+    x: 0,
+    y: 0,
+    fontSize,
+    anchor: 'left top',
+    attributes: { fill: '#5342F0', stroke: '#5342F0' },
+  };
+  return TextToSVG.loadSync(FONT).getSVG(text, svgOptions);
 };
 
 const handler = builder(ogpHandler);
